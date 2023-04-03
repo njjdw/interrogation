9dcab6f04668f7a692b4a15bd3b5d71fee682498
unknown <1613453205@qq.com>
2023-04-03 21:32:27 +0800

    删除文件夹

--- interrogation/src/utils/socket.js
+++
@@ -1,9 +0,0 @@
-export default{
-	
-	getSocket(){
-		let userInfo = localStorage.getItem("userInfo");
-		userInfo = JSON.parse(userInfo);
-		var websocket = new WebSocket("ws://127.0.0.1:88/interrogation/webSocket/"+userInfo.id);
-		return websocket
-	}
-}
